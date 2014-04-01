65cb316b1569a2325a823601aa0dd24d6bcf21fd
Tomasz Pewiński <pewniak747@gmail.com>
2014-04-01 14:15:14 +0200

    random leds

--- visualization.py
+++ visualization.py
@@ -1,10 +1,10 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 size = 4
 count = size ** 3
-cm = plt.get_cmap("RdYlGn")
 
 points = np.arange(0, count)
 points = [ [point / size ** 2, point / size % size, point % size] for point in points ]
@@ -12,7 +12,10 @@ points = [ [point / size ** 2, point / size % size, point % size] for point in p
 x = [ point[0] for point in points ]
 y = [ point[1] for point in points ]
 z = [ point[2] for point in points ]
-col = np.arange(len(x))
+
+leds = [ 1 if np.random.rand() > 0.5 else 0 for i in np.arange(0, count) ]
+
+col = [ [0.0, 0.0, 1.0, i] for i in leds ]
 
 fig = plt.figure()
 ax3D = fig.add_subplot(111, projection='3d')
